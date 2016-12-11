d982b7f633fa880d01ccebaad3012fb5729d29eb
Ricky Avina <rickyavina@icloud.com>
2016-12-10 23:36:03 -0800

    Added basic pong and basic Multipeer connectivity.

--- MultiPong/GameScene.swift
+++ MultiPong/GameScene.swift
@@ -8,82 +8,205 @@
 
 import SpriteKit
 import GameplayKit
+import MultipeerConnectivity
 
-class GameScene: SKScene {
+class GameScene: SKScene, MCSessionDelegate, MCBrowserViewControllerDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var ball = SKSpriteNode()
+    var enemy = SKSpriteNode()
+    var main = SKSpriteNode()
+    
+    var topLabel = SKLabelNode()
+    var bottomLabel = SKLabelNode()
+    
+    var startButton : SKNode! = nil
+    var gameStarted : Bool = false
+    
+    var score = [Int]()
+    
+    var peerID: MCPeerID!
+    var mcSession: MCSession!
+    var mcAdvertiserAssistant: MCAdvertiserAssistant!
     
     override func didMove(to view: SKView) {
+        peerID = MCPeerID(displayName: UIDevice.current.name)
+        mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
+        mcSession.delegate = self
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        startButton = SKSpriteNode(color: SKColor.blue, size: CGSize(width: 100, height: 44))
+        startButton.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        self.addChild(startButton)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
+        enemy = self.childNode(withName: "enemy") as! SKSpriteNode
+        main = self.childNode(withName: "main") as! SKSpriteNode
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        topLabel = self.childNode(withName: "topLabel") as! SKLabelNode
+        bottomLabel = self.childNode(withName: "bottomLabel") as! SKLabelNode
+        
+        ball.isHidden = true
+        enemy.isHidden = true
+        main.isHidden = true
+        topLabel.isHidden = true
+        bottomLabel.isHidden = true
+        
+        
+    }
+    
+    func startGame() {
+        
+        if mcSession.connectedPeers.count < 1 {
+            showConnectionPrompt()
+            print("People connected: \(mcSession.connectedPeers.count)")
+        } else {
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            ball.isHidden = false
+            enemy.isHidden = false
+            main.isHidden = false
+            topLabel.isHidden = false
+            bottomLabel.isHidden = false
+            startButton.isHidden = true
+            
+            ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20)) // give it initial push
+            
+            let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+            border.friction = 0
+            border.restitution = 1
+            
+            self.physicsBody = border
+            
+            score = [0,0]   // myScore, enemyScore
+            topLabel.text = "\(score[1])"
+            bottomLabel.text = "\(score[0])"
         }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+    func addScore(playerWhoWon : SKSpriteNode){
+        
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)     // remove all forces
+        
+        if playerWhoWon == main {
+            score[0]+=1
+            ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+        } else if playerWhoWon == enemy {
+            score[1]+=1
+            ball.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
         }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
+        topLabel.text = "\(score[1])"
+        bottomLabel.text = "\(score[0])"
+        
+        print("Your score: \(score[0])\nEnemy score: \(score[1])\n")
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for touch in touches {
+            let location = touch.location(in: self) // location of finger
+            
+            if startButton.contains(location){
+                print("Tapped!")
+                startGame()
+            }
+            
+            main.run(SKAction.moveTo(x: location.x, duration: 0.2))
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for touch in touches {
+            let location = touch.location(in: self) // location of finger
+            main.run(SKAction.moveTo(x: location.x, duration: 0.2))
+        }
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        enemy.run(SKAction.moveTo(x: ball.position.x, duration: 0.4))
+        
+        if ball.position.y <= main.position.y - 20 {
+            addScore(playerWhoWon: enemy)
+            
+            do {
+                var pos : Int = 1
+                let data = NSData(bytes: &pos, length: MemoryLayout<Int>.size)
+                try mcSession.send(data as Data, toPeers: mcSession.connectedPeers, with: .unreliable)
+            } catch let error as NSError{
+                let ac = UIAlertController(title: "Send error", message: error.localizedDescription, preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default))
+                let currentViewController :
+                    UIViewController=UIApplication.shared.keyWindow!.rootViewController!
+                currentViewController.present(ac, animated: true, completion: nil)
+            }
+
+            
+        } else if ball.position.y >= enemy.position.y + 70 {
+            addScore(playerWhoWon: main)
+        }
+        
+        
+    }
+    
+    func showConnectionPrompt(){
+        let ac = UIAlertController(title: "Connect to others", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Host a session", style: .default, handler: startHosting))
+        ac.addAction(UIAlertAction(title: "Join a session", style: .default, handler: joinSession))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        let currentViewController :
+            UIViewController=UIApplication.shared.keyWindow!.rootViewController!
+        currentViewController.present(ac, animated: true, completion: nil)
+    }
+    
+    func startHosting(action: UIAlertAction){
+        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "it-pong", discoveryInfo: nil, session: mcSession)
+        mcAdvertiserAssistant.start()
+    }
+    
+    func joinSession(action: UIAlertAction){
+        let mcBrowser = MCBrowserViewController(serviceType: "it-pong", session: mcSession)
+        mcBrowser.delegate = self
+        
+        let vc = self.view?.window!.rootViewController!
+        vc?.present(mcBrowser, animated: true)
+    }
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        switch state {
+        case .connected:
+            print("Connected: \(peerID.displayName)")
+            
+        case .connecting:
+            print("Connecting: \(peerID.displayName)")
+            
+        case .notConnected:
+            print("Not Connected: \(peerID.displayName)")
+        }
+        
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL, withError error: Error?) {
+        
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        print("DATA RECIEVED")
+    }
+    
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        browserViewController.dismiss(animated: true, completion: nil)
+
+    }
+    
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+      browserViewController.dismiss(animated: true, completion: nil)
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
     }
 }
